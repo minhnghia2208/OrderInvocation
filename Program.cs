4bc2c8bb9a294762a162c5fc8f06ba864ed89be2
Keith Pham <minhnghia2208@gmail.com>
2023-01-07 15:48:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+using System.Net.Http;
+
+static async Task InvokeEvent(HttpClient httpClient)
+{
+    using HttpResponseMessage response = await httpClient.PostAsync("https://localhost:4000/Order");
+
+    response.EnsureSuccessStatusCode();
+
+    var jsonResponse = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"{jsonResponse}\n");
+
+    // Expected output:
+    //   GET https://jsonplaceholder.typicode.com/todos/3 HTTP/ 1.1
+    //   {
+    //     "userId": 1,
+    //     "id": 3,
+    //     "title": "fugiat veniam minus",
+    //     "completed": false
+    //   }
+}
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+var clientOptions = new ServiceBusClientOptions()
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+client = new ServiceBusClient("Endpoint=sb://daprentity.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=iGY15mccd2kyP58QI+c/2y+ttvUS8vS4TVWdokZQOIc=", clientOptions);
+processor = client.CreateProcessor("order", new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
